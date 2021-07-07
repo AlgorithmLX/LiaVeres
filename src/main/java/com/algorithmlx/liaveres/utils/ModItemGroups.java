80f9bb0ff515ff20d1d0d0c0f814e32dda4edb6f
IgroGames2227 <izhukov2007@gmail.com>
2021-07-07 22:36:23 +0700

    Rename Matter_Crystal

--- src/main/java/com/algorithmlx/liaveres/utils/ModItemGroups.java
+++ src/main/java/com/algorithmlx/liaveres/utils/ModItemGroups.java
@@ -4,6 +4,7 @@ import com.algorithmlx.liaveres.init.ModItems;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.extensions.IForgeBlock;
 import net.minecraftforge.fml.RegistryObject;
 
 public class ModItemGroups {
@@ -17,5 +18,6 @@ public class ModItemGroups {
         };
     }
 
-    public static ItemGroup MOD_ITEM_TAB = createGroup("mod_item_tab", ModItems.MATER_CRYSTAL);
+    public static ItemGroup MOD_ITEM_TAB = createGroup("mod_item_tab", ModItems.MATTER_CRYSTAL);
+    public static  ItemGroup MOD_BLOCK_TAB = createGroup("mod_block_tab", ModItems.MATTER_CRYSTAL);
 }
